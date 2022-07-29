e27630abacabfe5721099f544ecfae8447345f03
Akshya Kumar KC <86335824+AkshyaKC@users.noreply.github.com>
2022-07-29 18:23:34 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ icon_size = 20
 
 st_button('linkedin', 'https://www.linkedin.com/in/akshya-kumar-kc-2ab404170/', 'Follow me on LinkedIn', icon_size)
 st_button('twitter', 'https://twitter.com/AkshyaKKC', 'Follow me on Twitter', icon_size)
-st_button('instagram', 'https://www.instagram.com/_akshya95_/', 'Follow me on Instagram', icon_size)
+st_button('instagram', 'https://www.instagram.com/cristiano/?hl=en', 'Follow me on Instagram', icon_size)
 st_button('medium', 'https://medium.com/@akshyakc095', 'Read my Blogs', icon_size)
