5bc6155ae5ba4039b2ac13d001d48cefe8e402bb
Akshya Kumar KC <86335824+AkshyaKC@users.noreply.github.com>
2022-07-29 18:08:30 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,15 @@ from PIL import Image
 load_css()
 
 col1, col2, col3 = st.columns(3)
-col2.image(Image.open('dp.png'))
+col2.image(Image.open('ak.jpg'))
 
-st.header('Chanin Nantasenamat, Ph.D.')
+st.header('Akshya Kumar KC')
 
-st.info('Developer Advocate, Content Creator and ex-Professor with an interest in Data Science and Bioinformatics')
+st.info('Data Analyst/Science Practitioner')
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('twitter', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
-st_button('linkedin', 'https://www.linkedin.com/in/chanin-nantasenamat/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('linkedin', 'https://www.linkedin.com/in/akshya-kumar-kc-2ab404170/', 'Follow me on LinkedIn', icon_size)
+st_button('twitter', 'https://twitter.com/AkshyaKKC', 'Follow me on Twitter', icon_size)
+st_button('instagram', 'https://www.instagram.com/_akshya95_/', 'Follow me on Instagram', icon_size)
+st_button('medium', 'https://medium.com/@akshyakc095', 'Read my Blogs', icon_size)
